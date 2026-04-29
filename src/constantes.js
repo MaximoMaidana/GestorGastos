76ce0ff3c04c61d68ed56ebd2fa3f9f036d7c206
Máximo Maidana <157309935+MaximoMaidana@users.noreply.github.com>
2026-04-29 21:39:47 +0000

    Crear archivos de componentes

---
+++ src/constantes.js
@@ -0,0 +1,7 @@
+export const LISTA_CATEGORIAS = [
+  { id: 'com', nombre: 'Comida', icono: '🍔' },
+  { id: 'tra', nombre: 'Transporte', icono: '🚌' },
+  { id: 'oci', nombre: 'Ocio', icono: '🎮' },
+  { id: 'sal', nombre: 'Salud', icono: '🏥' },
+  { id: 'edu', nombre: 'Educación', icono: '📚' }
+];
