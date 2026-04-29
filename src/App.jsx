186bdd1c403264ee1832604f305d616c73c0c872
Máximo Maidana <157309935+MaximoMaidana@users.noreply.github.com>
2026-04-29 21:31:09 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Balance from './Balance.jsx'
+import Formulario from './Formulario.jsx'
+import ListaMovimientos from './ListaMovimientos.jsx'
+import ModalPresupuesto from './ModalPresupuesto.jsx'
+import Filtros from './Filtros.jsx'
+
+
+function App() {
+}
